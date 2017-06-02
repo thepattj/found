e9bb9a6e9533d833b81869af4e9ee59340bac233
thepattj <jdej26@gmail.com>
2017-06-02 11:53:08 -0500

    creacion

---
+++ preguntas.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> LA OFERTA EDUCATIVA EXTRACURRICULAR DE ESCUELAS SECUNDARIAS DEL ESTADO DE QUERÉTARO </title>
+</head>
+<body>
+	<div class="titulo">Cuestionario para directivo y docentes</div>
+
+</body>
+</html>
